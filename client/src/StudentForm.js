0c738346c01a153f9ec9c58f79297bb35ec2285b
ElSnako314 <802913@sylvaniastudents.org>
2022-03-03 14:17:52 -0500

    Form Complete
    Student Form mostly complete (is not yet connected from database)

--- client/src/StudentForm.js
+++ client/src/StudentForm.js
@@ -1,59 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default class Login extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            username:'',
-            password:'',
-        };
-        //default information is null
-    }
-    updateInfo = (event) =>{
-            let fieldName = event.target.name;
-            let fieldValue = event.target.value;
-            if(fieldName === 'username') {
-                this.setState({username:fieldValue});
-            }
-            else if(fieldName === 'password'){
-                this.setState({password:fieldValue});
-            }
-            //updates variables when user updates the information
-    };
-    login=(e)=>{
-        let {username,password}=this.state;
-        fetch('/login', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                username:username,
-                password:password,
+const StudentForm = () => {
+    const [name, setName] = useState('')
+    const [free, setFree] = useState('')
+    const [subjects, setSubjects] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = () => {
+        const config = {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body":JSON.stringify({
+                name,
+                //free,
+                //subjects,
+                email
+                // Name, Email, isTutor
             })
-        })
-        //fetches login from server.js
-        .then(response=>response.json())
-        .then(data=>{
-            if(data) {
-                window.alert("You have successfully logged in. You can now access the events tab to join or create an event.")
-                this.handleLogin(data)
-            }
-            else window.alert("Try Again. Either your username or password is incorrect.")
-        })
-        //shoots the user a message saying if their login was successful or not
+        }
+        fetch("/registerTutor", config)
     }
-    handleLogin = (isLoggedIn) => {
-        this.props.tryLogin(isLoggedIn);
-    }
-    render(){
-        return(
-            <div className="login">
-                <label for="username">Username:</label>
-                <input onChange={this.updateInfo} type="text" name="username" value={this.state.username} required/><br /><br />
-                <label for="password">Password:</label>
-                <input onChange={this.updateInfo} type="password" name="password" value={this.state.password} required/><br /><br />
-                <button onClick={this.login}>Login</button>
-            </div>
-        )
-        //HTML for the form
-    }
+
+    return (
+        <div>
+            <label htmlFor='name'>
+                Name
+            </label>
+            <input
+                onChange={(e) => {
+                    setName(e.target.value)
+                }}
+                type="text"
+                placeholder='Your Name'
+                name='name'
+            />
+            <label htmlFor='email'>
+                Email
+            </label>
+            <input
+                onChange={(e) => {
+                    setEmail(e.target.value)
+                }}
+                type="email"
+                placeholder='Your Email'
+                name='email'
+            />
+            <input
+                onClick={handleSubmit}
+                type="submit"
+                value='Register'
+            />
+        </div>
+    )
 }
+
+export default StudentForm;
