538f8ecbdbd0b2515563ef2d68076b0c233911c5
saraiyakrs <718618@sylvaniastudents.org>
2022-03-03 14:22:20 -0500

    other tings

--- client/src/Components/Tabs/Home.js
+++ client/src/Components/Tabs/Home.js
@@ -1,8 +1,8 @@
 function Home() {
     return (
         <div>
-            <h1>Welcome to The Northview High School Tutoring Page</h1>
-           
+            <h1 id = "HomeTitle">Welcome to The Northview High School Tutoring Page!</h1>
+            <h2 id = "InfoHome">Tutoring Sessions Should be displayed here</h2>
         </div>
     )
 }
--- server.js
+++ server.js
@@ -4,3 +4,30 @@ const db = require('better-sqlite3')('NVTutoring.db')
 
 app.use(express.static("build"))
 app.use(express.json())
+
+
+
+app.get('/tutor_sessions', (req, res) => {
+    const query = db.prepare("SELECT * FROM tutor_sessions")
+    const tutor_sessions = query.all()
+    res.json({
+        tutor_sessions
+    })
+})
+
+
+app.get('/users', (req, res) => {
+    const query = db.prepare("SELECT * FROM users")
+    const users = query.all()
+    res.json({
+        users
+    })
+})
+
+
+
+
+
+app.listen(8080, () => {
+    console.log("server started")
+})
