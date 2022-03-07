060fe64db773adf2919d3a9cc5ccd04ac6a8940b
Liam Gallagher <802189@sylvaniastudents.org>
2022-03-07 13:41:45 -0500

    Created Forms, New Navbar Item

--- client/src/Components/TabManager.js
+++ client/src/Components/TabManager.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import Home from "./Tabs/Home";
 import Contact from "./Tabs/Contact";
 import Profile from "./Tabs/Profile";
-import GoogleLoginTab from "./Tabs/GoogleLoginTab"
+import GoogleLoginTab from "./Tabs/GoogleLoginTab";
+import Form from "./Tabs/Form";
 
 function TabManager() {
     const [activeTab, setActiveTab] = useState(<Home />);
@@ -19,14 +20,18 @@ function TabManager() {
     const handleGoogleLogin = () => {
         setActiveTab(<GoogleLoginTab />)
     }
+    const handleForm = () => {
+        setActiveTab(<Form />)
+    }
 
     return (
         <div id="navbar">
             <ul>
                 <li><a href="" onClick={handleHome}>Home</a></li>
-                <li><a href="#contact" onClick={handleContact}>Contact Us</a></li>
+                <li><a href="#form" onClick={handleForm}>Form</a></li>
                 <li><a href="#profile" onClick={handleProfile}>Profile</a></li>
                 <li><a href="#google" onClick={handleGoogleLogin}>Login With Google</a></li>
+                <li><a href="#contact" onClick={handleContact}>Contact Us</a></li>
             </ul>
             {/*HTML for the navbar*/}
 
