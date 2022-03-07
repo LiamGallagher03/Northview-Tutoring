6195d34dae3ffb014ea679ab59586f8bb106bbbd
Liam Gallagher <802189@sylvaniastudents.org>
2022-03-07 13:59:23 -0500

    Updated Contact page

--- client/src/Components/Tabs/Contact.js
+++ client/src/Components/Tabs/Contact.js
@@ -1,13 +1,8 @@
 function Contact() {
     return (
         <div>
-            <h1>Contact Us!</h1>
-            <h2>EMAIL :</h2>
-            <h2>FACEBOOK : </h2>
-            <h2>INSTAGRAM : </h2>
-            <br></br>
-            <br></br>
-            <h3>Let us know if you have any questions about our convention center and/or any problems that you have!</h3>
+            <h1>Contact Frank, Krishna, or Liam.</h1>
+            
 
         </div>
     )
