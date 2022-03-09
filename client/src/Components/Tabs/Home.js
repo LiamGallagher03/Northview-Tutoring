acd06aeb8409409544f3895b5edf4fae831b49eb
Liam Gallagher <802189@sylvaniastudents.org>
2022-03-09 14:18:34 -0500

    tutor table CSS/HTML

--- client/src/Components/Tabs/Home.js
+++ client/src/Components/Tabs/Home.js
@@ -1,8 +1,42 @@
+import { Table } from 'reactstrap';
+
+
 function Home() {
     return (
         <div>
-            <h1 id = "homeTitle">Welcome to The Northview High School Tutoring Page!</h1>
-            <h2 id = "infoHome">Tutoring Sessions Should be displayed here</h2>
+            <div>
+                <h1 id="homeTitle">Welcome to The Northview High School Tutoring Page!</h1>
+            </div>
+            <div id="tutorList">
+                <Table bordered={true}>
+                    <thead>
+                        <tr>
+                            <th>
+                                Head 1
+                            </th>
+                            <th>
+                                Head 2
+                            </th>
+                            <th>
+                                Head 3
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    Muck
+                                </td>
+                                <td>
+                                    Suck
+                                </td>
+                                <td>
+                                    Yuck
+                                </td>
+                            </tr>
+                        </tbody>
+                </Table>
+            </div>
         </div>
     )
 }
